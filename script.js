12fabba2b1519ddf3575a1c3dc1ae8d80bbb4c72
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-20 16:43:50 +0100

    Added last functionality in js. When modal is visible and when you click on th outsie then modal is disapearing

--- script.js
+++ script.js
@@ -17,3 +17,7 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal');
 });
+
+// hide modal on outside click
+window.addEventListener('click', e => e.target === modal ?
+modal.classList.remove('show-modal') : false);
