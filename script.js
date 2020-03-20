c121400a1e122a0e4c7aab7e51dcb4f1283ceaa7
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-20 16:21:42 +0100

    Added functionality ables to toggle class to body. It ables to write on and write off the class show-nav to the body element

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
+
+// toggle navidation
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+});
